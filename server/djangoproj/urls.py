9cdb6c77fb15fb4d7323dbc715e9edadd1441fe7
Refrizor <retizao.tv@gmail.com>
2026-05-28 15:53:40 -0400

    updates

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -28,5 +28,4 @@ urlpatterns = [
                   path('login/', TemplateView.as_view(template_name="index.html")),
                   path('logout/', TemplateView.as_view(template_name="index.html")),
                   path('register/', TemplateView.as_view(template_name="index.html")),
-
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
