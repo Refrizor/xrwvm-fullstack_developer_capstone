d09b30fccc7714f3bfaf7d4c4de453816d7ad447
Refrizor <retizao.tv@gmail.com>
2026-05-29 21:28:11 -0400

    clean up project

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,8 @@
 # Uncomment the following imports before adding the Model code
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+
+
 # from django.utils.timezone import now
 # from django.core.validators import MaxValueValidator, MinValueValidator
 
@@ -16,8 +18,10 @@ from django.db import models
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+
     def __str__(self):
         return self.name
+
 
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,17 +1,11 @@
-# Uncomment the required imports before adding the code
-
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
-from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
-from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
-
-from django.http import JsonResponse
-from django.contrib.auth import login, authenticate
 import logging
 import json
+
+import requests
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
+from django.http import JsonResponse
+from django.contrib.auth import login, authenticate
 from django.views.decorators.csrf import csrf_exempt
 
 from .models import CarMake, CarModel
@@ -51,7 +45,6 @@ def logout_request(request):
 # Create a `registration` view to handle sign up request
 @csrf_exempt
 def registration(request):
-    context = {}
     data = json.loads(request.body)
 
     username = data['userName']
@@ -65,12 +58,19 @@ def registration(request):
         # If user exist
         User.objects.get(username=username)
         username_exist = True
-    except:
+
+        User.objects.get(email=email)
+        email_exist = True
+
+    except User.DoesNotExist:
         logger.debug("{} is new user".format(username))
 
-    if not username_exist:
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
-                                        password=password, email=email)
+    if not username_exist and not email_exist:
+        user = User.objects.create_user(username=username,
+                                        first_name=first_name,
+                                        last_name=last_name,
+                                        password=password,
+                                        email=email)
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
@@ -82,12 +82,13 @@ def registration(request):
 def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
-    if (count == 0):
+    if count == 0:
         initiate()
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append({"CarModel": car_model.name,
+                     "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
 
@@ -117,7 +118,7 @@ def get_dealer_reviews(request, dealer_id):
 
 
 def get_dealer_details(request, dealer_id):
-    if (dealer_id):
+    if dealer_id:
         endpoint = "/fetchDealer/" + str(dealer_id)
         dealership = get_request(endpoint)
         return JsonResponse({"status": 200, "dealer": dealership})
@@ -126,12 +127,12 @@ def get_dealer_details(request, dealer_id):
 
 
 def add_review(request):
-    if (request.user.is_anonymous == False):
-        data = json.loads(request.body)
+    if not request.user.is_anonymous:
         try:
-            response = post_review(data)
+            data = json.loads(request.body)
+            post_review(data)
             return JsonResponse({"status": 200})
-        except:
+        except requests.exceptions.RequestException:
             return JsonResponse({"status": 401, "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
